06e0b2b21aa40db20a7b6e1a367e06441f2f2429
Nimai Jariwala <nimai.jariwala.1@ens.etsmtl.ca>
2023-01-27 15:38:20 +0000

    prelim analysis done

--- src/provider_depth_node.cc
+++ src/provider_depth_node.cc
@@ -2,7 +2,7 @@
  * \file	provider_depth_node.cc
  * \author	Francis Alonzo
  * \date	24/07/2017
- * 
+ *
  * \copyright Copyright (c) 2021 S.O.N.I.A. All rights reserved.
  *
  * \section LICENSE
@@ -35,14 +35,14 @@ namespace provider_depth
         depthPublisher_ = nh_->advertise<std_msgs::Float32>("/provider_depth/depth", 100);
         pressPublisher_ = nh_->advertise<std_msgs::Float32>("/provider_depth/press", 100);
         tempPublisher_ = nh_->advertise<std_msgs::Float32>("/provider_depth/temp", 100);
-        
+
         // Thread for Seral Device connection.
         readThread = std::thread(std::bind(&ProviderDepthNode::readSerialDevice, this));
         // Thread for CheckSum function thingy.
         sendID1Thread = std::thread(std::bind(&ProviderDepthNode::sendId1Register, this));
 
         serialConnection_.flush();
-        
+
         // Start host server.
         tare_srv = nh_->advertiseService("/provider_depth/tare", &ProviderDepthNode::tare, this);
     }
@@ -55,9 +55,10 @@ namespace provider_depth
     void ProviderDepthNode::Spin()
     {
         // TODO: Make the 100 some kind of const or part of the ctor.
-        ros::Rate r(100); // 100 Hz 
+        // r is a bad variable name.
+        ros::Rate r(100); // 100 Hz
 
-        while(ros::ok())
+        while (ros::ok())
         {
             ros::spinOnce();
             r.sleep();
@@ -66,33 +67,46 @@ namespace provider_depth
 
     void ProviderDepthNode::readSerialDevice()
     {
+        // TODO: Make the 100 some kind of const or part of the ctor.
+        // r is a bad variable name.
         ros::Rate r(100); // 100 Hz
         char buffer[BUFFER_SIZE];
         ROS_INFO_STREAM("Serial Read Thread is started");
 
-        while(!ros::isShuttingDown())
+        while (!ros::isShuttingDown())
         {
             // find the message beginning
             do
             {
                 serialConnection_.readOnce(buffer, 0);
-            }while(buffer[0] != '$');
+            } while (buffer[0] != '$');
 
+            // internal counter
             int i;
 
-            for(i = 1; buffer[i-1] != '\n' && i < BUFFER_SIZE; i++)
+            /**
+             * Set the counter to 1,
+             * While the last index of the buffer is not '\n' AND the counter is less than the buffer
+             * post-increment i
+             *
+             * This fills the buffer one char at a time and checks against
+             * the end char or max buffer size.
+             */
+            for (i = 1; buffer[i - 1] != '\n' && i < BUFFER_SIZE; i++)
             {
                 serialConnection_.readOnce(buffer, i);
             }
 
-            if(i >= BUFFER_SIZE)
+            // if the buffer is maxed out, skip the rest of the while loop.
+            if (i >= BUFFER_SIZE)
             {
                 continue;
             }
 
+            // set the last char (ideally the '\n') to 0
             buffer[i] = 0;
 
-            if(!strncmp(&buffer[1], ID1, 5)) // Add checksum verification
+            if (!strncmp(&buffer[1], ID1, 5)) // Add checksum verification
             {
                 std::unique_lock<std::mutex> mlock(id1_mutex);
                 id1_string = std::string(buffer);
@@ -106,10 +120,11 @@ namespace provider_depth
     void ProviderDepthNode::sendId1Register()
     {
         std::string tmp = "";
-        
+
         ROS_INFO_STREAM("Send ID1 Thread started");
 
-        while(!ros::isShuttingDown())
+        // Why !ros::isShuttingDown() and not ros::ok()
+        while (!ros::isShuttingDown())
         {
             tmp = "";
             std::unique_lock<std::mutex> mlock(id1_mutex);
@@ -117,7 +132,7 @@ namespace provider_depth
 
             try
             {
-                if(!id1_string.empty()) // Add checksum verification
+                if (!id1_string.empty()) // Add checksum verification
                 {
                     std::stringstream ss(id1_string);
 
@@ -134,17 +149,16 @@ namespace provider_depth
                     pressPublisher_.publish(press_);
 
                     std::getline(ss, tmp, ','); // skip B
-                    
+
                     std::getline(ss, tmp, ','); // Get the temperature
                     temp_.data = stof(tmp);
                     tempPublisher_.publish(temp_);
                 }
             }
-            catch(...)
+            catch (...) // BAD Add Exception
             {
                 ROS_ERROR_STREAM("Issue with the message received from depth sensor");
             }
-            
         }
     }
 
@@ -152,6 +166,8 @@ namespace provider_depth
     {
         serialConnection_.transmit("#tare\n");
         ros::Duration(0.1).sleep();
+
+        // Confirmation of tare?
 
         ROS_INFO("Depth Sensor tare finished");
         return true;
